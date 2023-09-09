6c828244fedf04610520014f90f4346d06f471ad
Guilhermecocato <144507493+Guilhermecocato@users.noreply.github.com>
2023-09-09 10:43:16 -0300

    Problemas

---
+++ Problema2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float distancia, litrosgastos, consumomedio;
+    
+    //DADOS UMA DISTANCIA E O TOTAL DE LITROS DE COMBUSTIVEL GASTO POR UM AUTOMOVEL PARA PERCORRE-LA, INFORME O CONSUMO MEDIO
+
+    printf("CALCULE O CONSUMO MEDIO DE COMBUSTIVEL");
+    
+    printf("\n\nINSIRA A DISTANCIA(KM) = ");
+    scanf("%f", &distancia);
+    printf("TOTAL DE LITROS GASTOS = ");
+    scanf("%f", &litrosgastos);
+    
+    consumomedio = (distancia / litrosgastos);
+    printf("\nCONSUMO MEDIO DE COMBUSTIVEL = %4.2f LITROS POR KM", consumomedio);
+    
+    return 0;
+}
+
+
