f98fa21e779b48d32e440fc66b86bcafa2f75ff3
Guilhermecocato <144507493+Guilhermecocato@users.noreply.github.com>
2023-09-09 09:52:52 -0300

    Problemas

---
+++ Problema1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+	float primeiranota, segundanota, media;
+	
+	//DADAS AS DUAS NOTAS DE UM ALUNO,INFORME A SUA MEDIA FINAL;
+	
+	printf("CALCULE A MEDIA:");
+	
+	printf("\n\nPRIMEIRA NOTA = ");
+	scanf("%f", &primeiranota);
+	printf("SEGUNDA NOTA = ");
+	scanf("%f", &segundanota);
+	
+	media = (primeiranota + segundanota) / 2;
+	printf("\nMEDIA = %4.2f\n", media);
+	
+	return(0);
+}
