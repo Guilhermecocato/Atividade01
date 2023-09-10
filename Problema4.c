73abf632d1b5dad26cb0d051e84d30a41e252f30
Guilhermecocato <144507493+Guilhermecocato@users.noreply.github.com>
2023-09-10 11:35:33 -0300

    Problemas

---
+++ Problema4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float temperatura, celsius;
+    
+    //Dada um temperatura em graus Fahrenheit, informe o valor correspondente em graus Celsius
+    
+    printf("INFORME UMA TEMPERATURA EM GRAU FAHRENHEIT\nE CONVERTA EM GRAUS CELSIUS");
+    
+    printf("\n\nGRAU FAHRENHEIT(ºF) = ");
+    scanf("%f", &temperatura);
+    
+    celsius = (temperatura - 32) * 5/9;
+    printf("\nGRAUS CELSIUS = %fºC", celsius);
+
+    return 0;
+}
